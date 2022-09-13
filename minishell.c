4420d04f3a2bc03dcb49220e0fd57956b8d9257f
Chloe AUBRY <caubry@e1r7p7.clusters.42paris.fr>
2022-09-13 12:19:03 +0200

    debut norme minihshell.c

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: caubry <caubry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 19:32:01 by caubry            #+#    #+#             */
-/*   Updated: 2022/09/13 11:26:49 by caubry           ###   ########.fr       */
+/*   Updated: 2022/09/13 12:15:51 by caubry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,31 +30,30 @@
 //     first = NULL;
 // }
 
-int ft_echo(char **cmd, int i, int n)
+int	ft_echo(char **cmd, int i, int n)
 {
-    if (cmd[1] && ft_strcmp(cmd[1], "-n"))
-    {
-        i++;
-        n = 1;
-    }
-    while (cmd[i])
-    {
-        if ((i > 1 && n == 0) || i > 2)
-            printf(" ");
-        // printf("%s", cmd[i]);
-        i++;
-    }
-    if (!n)
-        printf("\n");
-    return (1);
+	if (cmd[1] && ft_strcmp(cmd[1], "-n"))
+	{
+		i++;
+		n = 1;
+	}
+	while (cmd[i])
+	{
+		if ((i > 1 && n == 0) || i > 2)
+			printf(" ");
+		i++;
+	}
+	if (!n)
+		printf("\n");
+	return (1);
 }
 
-int ft_cmd(char **cmd)
+int	ft_cmd(char **cmd)
 {
-    if (!cmd[0])
-        return(0);
-    if (ft_strcmp(cmd[0], "echo"))
-        ft_echo(cmd, 1, 0);
+	if (!cmd[0])
+		return (0);
+	if (ft_strcmp(cmd[0], "echo"))
+		ft_echo(cmd, 1, 0);
     // else if (ft_strncmp(cmd[0], "cd"))
     //     ft_cd(cmd);
     // else if (ft_strncmp(cmd[0], "pwd"))
@@ -67,102 +66,102 @@ int ft_cmd(char **cmd)
     //     ft_env(cmd);
     // else if (ft_strncmp(cmd[0], "exit"))
     //     ft_exit(cmd);
-    else
-        printf("Command '%s' not found\n", cmd[0]);
-    return (1);
+	else
+		printf("Command '%s' not found\n", cmd[0]);
+	return (1);
 }
 
-int ft_erreur(int erreur)
+int	ft_erreur(int erreur)
 {
-    if (erreur == 1)
-        printf("Il ne faut pas mettre d'arguments\n");
-    return (0);
+	if (erreur == 1)
+		printf("Il ne faut pas mettre d'arguments\n");
+	return (0);
 }
 
-void  ft_printcmd(t_cmd **first)
+void	ft_printcmd(t_cmd **first)
 {
-    t_cmd   *lst;
-    int     i;
+	t_cmd	*lst;
+	int		i;
 
-    lst = *first;
-    i = 0;
-    while (lst && lst->arg[i])
-    {
-        while (lst->arg[i])
-        {
-            if (i == 0)
-                printf("Nouvelle commande = ");
-            printf("%s ", lst->arg[i]);
-            i++;
-        }
-        printf("\n");
-        lst = lst->next;
-        i = 0;
-    }
+	lst = *first;
+	i = 0;
+	while (lst && lst->arg[i])
+	{
+		while (lst->arg[i])
+		{
+			if (i == 0)
+				printf("Nouvelle commande = ");
+			printf("%s ", lst->arg[i]);
+			i++;
+		}
+		printf("\n");
+		lst = lst->next;
+		i = 0;
+	}
 }
 
-void ft_initcmd(t_cmd **first, char **cmd)
+void	ft_initcmd(t_cmd **first, char **cmd)
 {
-    int i;
-    int j;
-    int dup;
+	int	i;
+	int	j;
+	int	dup;
 
-    i = 0;
-    while (cmd[i])
-    {
-        j = 0;
-        dup = i;
-        while (cmd[i] && ft_strcmp(cmd[i], "|"))
-        {
-            i++;
-            j++;
-        }
-        ft_lstadd_back(first, ft_lstnew(j, cmd, dup));
-        j = 0;
-        if (cmd[i])
-            i++;
-    }
+	i = 0;
+	while (cmd[i])
+	{
+		j = 0;
+		dup = i;
+		while (cmd[i] && ft_strcmp(cmd[i], "|"))
+		{
+			i++;
+			j++;
+		}
+		ft_lstadd_back(first, ft_lstnew(j, cmd, dup));
+		j = 0;
+		if (cmd[i])
+			i++;
+	}
 }
 
-void ft_minishell(char *line)
+void	ft_minishell(char *line)
 {
-    t_cmd *first;
-    char **cmd;
+	t_cmd	*first;
+	char	**cmd;
 
-    first = NULL;
-    cmd = ft_split(line);
-    if (!cmd)
-        printf("Erreur de saisie\n");
-    ft_initcmd(&first, cmd);
-    ft_printcmd(&first);
-    free(first);
+	first = NULL;
+	cmd = ft_split(line);
+	if (!cmd)
+		printf("Erreur de saisie\n");
+	ft_initcmd(&first, cmd);
+	ft_printcmd(&first);
+	free(first);
 }
 
-int main(int ac, char **av, char **envp)
+int	main(int ac, char **av, char **envp)
 {
-    static char *line;
-    int quit;
-    (void)**envp;
-    int i;
+	static char	*line;
+	int		quit;
+	(void)		**envp;
+	int		i;
 
-    line = NULL;
-    quit = 0;
-    i = 0;
-    if (ac > 1 && av[0]) 
-        return (ft_erreur(1));
-    while (!quit)
-    {
-        if (line)
-            free(line);
-        line = readline("?>");
-        if (line && *line)
-            add_history(line);
-        ft_minishell(line);
-    }
-    while (envp[i])
-    {
-        printf("%s\n",envp[i]);
-        i++;
-    }
-    return (0);
+	line = NULL;
+	quit = 0;
+	i = 0;
+	if (ac > 1 && av[0]) 
+		return (ft_erreur(1));
+	while (!quit)
+	{
+		if (line)
+			free(line);
+		line = readline("?>");
+		if (line && *line)
+		add_history(line);
+		ft_minishell(line);
+	}
+	while (envp[i])
+	{
+		printf("%s\n",envp[i]);
+		i++;
+	}
+	return (0);
 }
